71b40942098ead26deb05d6ecab49ef5ad2203d0
wenyu <wenyu@wustl.edu>
2019-05-27 11:47:43 -0700

    Updates

---
+++ precache-manifest.cc6a2899252a9249953c9c9ffbe334c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf87a71c39c1662ebe71f59d4ec78a52",
+    "url": "/delivery-react/index.html"
+  },
+  {
+    "revision": "dd536670228a2e16a337",
+    "url": "/delivery-react/static/css/2.1511f479.chunk.css"
+  },
+  {
+    "revision": "f98bc54c472ff2fde451",
+    "url": "/delivery-react/static/css/main.b1081585.chunk.css"
+  },
+  {
+    "revision": "dd536670228a2e16a337",
+    "url": "/delivery-react/static/js/2.64721a44.chunk.js"
+  },
+  {
+    "revision": "f98bc54c472ff2fde451",
+    "url": "/delivery-react/static/js/main.e39eb0fc.chunk.js"
+  },
+  {
+    "revision": "40260c5cd467ef32c9a0",
+    "url": "/delivery-react/static/js/runtime~main.5ce49262.js"
+  },
+  {
+    "revision": "e799376296fe4021f14458da96c04fb7",
+    "url": "/delivery-react/static/media/background.e7993762.png"
+  }
+]);
