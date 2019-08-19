302c6465bfd613fec27d5e25f126107661f97531
wenyu <wenyu@wustl.edu>
2019-08-18 21:10:59 -0700

    websoket

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect, useContext, useReducer } from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 import { Nav, Intro, Info } from "./component";
 import Context from "./context";
 import reducer from "./reducer";
 import "./App.scss";
+
+const wsLink = new WebSocketLink({
+  uri: "wss://delivery-my-settle-down.herokuapp.com/graphql",
+  options: {
+    reconnect: true
+  }
+});
+
+const client = new ApolloClient({
+  link: wsLink,
+  cache: new InMemoryCache()
+});
 
 function App() {
   const [navClass, setNavClass] = useState("");
@@ -22,24 +38,26 @@ function App() {
   }, []);
 
   return (
-    <Context.Provider value={{ state, dispatch }}>
-      <Nav navClass={navClass} />
-      <ReactFullpage
-        anchors={["firstPage", "secondPage"]}
-        render={({ state, fullpageApi }) => {
-          return (
-            <>
-              <div className="section" id="background">
-                <Intro fullpageApi={fullpageApi} />
-              </div>
-              <div className="section">
-                <Info />
-              </div>
-            </>
-          );
-        }}
-      />
-    </Context.Provider>
+    <ApolloProvider client={client}>
+      <Context.Provider value={{ state, dispatch }}>
+        <Nav navClass={navClass} />
+        <ReactFullpage
+          anchors={["firstPage", "secondPage"]}
+          render={({ state, fullpageApi }) => {
+            return (
+              <>
+                <div className="section" id="background">
+                  <Intro fullpageApi={fullpageApi} />
+                </div>
+                <div className="section">
+                  <Info />
+                </div>
+              </>
+            );
+          }}
+        />
+      </Context.Provider>
+    </ApolloProvider>
   );
 }
 
